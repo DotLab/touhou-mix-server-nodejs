79a1281366e3c044422688f14d995fc666633464
Kailang <kailangfu@gmail.com>
2019-03-12 12:56:36 -0500

    Add basic connection

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,12 +7,12 @@ module.exports = {
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
+
     env: {
       NODE_ENV: 'production',
     },
-    env_development: {
-      NODE_ENV: 'development',
-      DEBUG: 'thmix*',
+    env_staging: {
+      NODE_ENV: 'staging',
     },
   }],
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,52 @@
 const debug = require('debug')('thmix');
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/thmix', {useNewUrlParser: true});
-mongoose.set('useFindAndModify', false);
-const ObjectId = mongoose.Schema.Types.ObjectId;
+const env = process.env.NODE_ENV;
+let port = '6003';
+let database = 'thmix';
+if (env === 'staging') {
+  port = '6004';
+  database = 'thmix-staging';
+}
+debug('running as', env, 'on port', port, 'using database', database);
 
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb://localhost:27017/${database}`, {useNewUrlParser: true});
+mongoose.set('useFindAndModify', false);
+
+// const ObjectId = mongoose.Schema.Types.ObjectId;
+// const User = mongoose.model('User', {
+//   name: String,
+//   salt: String,
+//   hash: String,
+// });
+
+const io = require('socket.io')(port);
+
+const VERSION = 0;
+const INTENT_WEB = 'web';
+
+function success(data) {
+  return {success: true, data};
+}
+
+// function error(data) {
+//   return {error: true, data};
+// }
+
+io.on('connection', function(socket) {
+  debug('connection', socket.id);
+
+  socket.on('cl_handshake', (info, done) => {
+    debug('cl_handshake', socket.id, info);
+
+    if (info.version !== VERSION) {
+      socket.disconnect();
+      return;
+    }
+
+    // if (info.intent === INTENT_WEB) {
+    // }
+
+    done(success({version: VERSION, intent: INTENT_WEB}));
+  });
+});
