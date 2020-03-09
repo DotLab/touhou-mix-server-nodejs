1880ad5250f7bed19bce7afdfb3829e4a5239d19
Kailang <kailangfu@gmail.com>
2020-03-09 15:11:22 -0500

    Remove staging from pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps: [{
-    name: 'thmix-server',
+    name: 'thmix-sv',
     script: './src/index.js',
 
     instances: 1,
@@ -10,18 +10,6 @@ module.exports = {
 
     env: {
       NODE_ENV: 'production',
-    },
-  }, {
-    name: 'thmix-server-staging',
-    script: './src/index.js',
-
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-
-    env: {
-      NODE_ENV: 'staging',
     },
   }],
 };
