434edfa9b891b224c20dcef8bb3f998babef9861
hinataizayoi <hinataizayoi@gmail.com>
2020-04-08 16:16:29 -0500

    Save status

--- src/SocketIoSession.js
+++ src/SocketIoSession.js
@@ -237,6 +237,9 @@ module.exports = class SocketIoSession {
     this.socket.on('cl_web_midi_recently_played', this.onClWebMidiRecentlyPlayed.bind(this));
     this.socket.on('cl_web_midi_play_history', this.onClWebMidiPlayHistory.bind(this));
 
+    this.socket.on('ClWebMidiDerivedFrom', createRpcHandler(this.onClWebMidiDerivedFrom.bind(this)));
+    this.socket.on('ClWebMidiSupersede', createRpcHandler(this.onClWebMidiSupersede.bind(this)));
+
     this.socket.on('cl_web_soundfont_get', this.onClWebSoundfontGet.bind(this));
     this.socket.on('cl_web_soundfont_list', this.onClWebSoundfontList.bind(this));
     this.socket.on('cl_web_soundfont_upload', this.onClWebSoundfontUpload.bind(this));
@@ -1376,5 +1379,23 @@ module.exports = class SocketIoSession {
     debug('  onClWebDocCommentList', docId);
 
     return await commentController.list({docId});
+  }
+
+  async onClWebMidiDerivedFrom({derivedFromId}) {
+    derivedFromId = new ObjectId(derivedFromId);
+    debug('  onClWebMidiDerivedFrom', derivedFromId);
+
+    const derived = await Midi.findById(derivedFromId);
+    if (!derived) throw codeError(0, 'not found');
+    return serializeMidi(derived);
+  }
+
+  async onClWebMidiSupersede({supersedeId}) {
+    supersedeId = new ObjectId(supersedeId);
+    debug('  onClWebMidiSupersede', supersedeId);
+
+    const midi = await Midi.findById(supersedeId);
+    if (!midi) throw codeError(0, 'not found');
+    return serializeMidi(midi);
   }
 };
